21bbc6283a18b9d76ba0fd5486ca48d8530d6017
ravishagoodpegg-lgtm <ravisha.goodpegg@gmail.com>
2026-05-20 13:28:08 +0530

    changes made

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {saveUser , deleteUser, loginUser} from '../controllers/UserController.js'
+import {saveUser , deleteUser, loginUser} from '../controllers/userController.js'
 
 const router = express.Router();
 router.post("/register",saveUser)
